9209dc77a272111da1c9231b0fa7ea9dc77f04c3
Dan Luh <azndervish@gmail.com>
2025-04-24 01:04:24 -0400

    Configure next.config.js for static exports to the out directory for GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  trailingSlash: true, // This adds a trailing slash to all routes
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.test\.js$/,
@@ -8,6 +10,9 @@ const nextConfig = {
     });
     return config;
   },
+
+  // Static export configuration
+  distDir: 'out', // Define the output directory
 };
 
 module.exports = nextConfig;
